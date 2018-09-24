5e7655a8a2c121936b16670c794eba4daeda49e3
Mohammad Abdul Bari <mabari@thoughtworks.com>
2018-09-25 00:19:49 +0530

    Generate correct combinations for 6 people

--- pairsGenerator.py
+++ pairsGenerator.py
@@ -37,12 +37,43 @@ class PairGenerator():
                         remaining_pairs.remove(current_pair)
                         j -= 1
                     j += 1
-                print(combination)
                 combinations_with_current_pair.append(combination)
             all_combinations.append(combinations_with_current_pair)
-        return all_combinations
+        valid_combinations = []
+        for list1 in all_combinations:
+            for list2 in list1:
+                if(len(list2) == len(this.list_of_people) / 2):
+                    valid_combinations.append(list2)
+        return valid_combinations
+
+    def can_add_list(this, first_list, second_list):
+        for first_pair in first_list:
+            for second_pair in second_list:
+                if first_pair == second_pair:
+                    return False
+        return True
+
+    def can_add_to_big_list(this, big_list, second_list):
+        for a_list in big_list:
+            if this.can_add_list(a_list, second_list) == False:
+                return False
+        return True
+
+    def final_combinations(this):
+        final_combinations = []
+        all_combinations = this.generate_all_combinations()
+        for i in range(0, len(all_combinations)):
+            temporory_combinations = [all_combinations[i]]
+            for j in range(i + 1, len(all_combinations)):
+                current_combination = all_combinations[j]
+                if(this.can_add_to_big_list(temporory_combinations, current_combination)):
+                    temporory_combinations.append(current_combination)
+            if(len(temporory_combinations) == len(this.list_of_people) - 1):
+                final_combinations.append(temporory_combinations)
+        return final_combinations
 
 pairGenerator = PairGenerator(["A", "B", "C", "D", "E", "F"])
-all_combinations = pairGenerator.generate_all_combinations()
-# for combination in all_combinations:
-#     print(combination)
+final_combinations = pairGenerator.final_combinations()
+print(final_combinations)
+
+        
